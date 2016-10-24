71f62c58cf5c9d427c56ef6fd2fa86c9d473030f
mente <mente@nx.local>
2016-10-24 21:53:27 +0900

    project create

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
